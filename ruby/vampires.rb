d97c2ea54416591144290e48ae30c31db86a363f
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-08-14 19:26:53 -0700

    add true and false booleans to g_bread and health_ins

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,13 +11,30 @@ g_bread = gets.chomp
 p "Would you like to enroll in the company's health insurance?(y/n)"
 health_ins = gets.chomp
 
-#update variables as each condition is checked.
-if user_year.to_i != current_year - user_age.to_i && g_bread == "n"
-    user_status = "probably a vampire..."
-else
-	p "idk fam"
+if g_bread == "y"
+	true
+elsif g_bread == "n"
+	false
 end
 
+if health_ins == "y"
+	true
+elsif health_ins == "n"
+	false
+end
+
+#update variables as each condition is checked.
+if user_year.to_i != current_year - user_age.to_i && (g_bread == "n" && health_ins == "n")
+    user_status = "Almost certainly a vampire!"
+elsif user_name == "Drake Cula" || "Tu Fang"
+	user_status = "Almost certainly a vampire!"
+elsif user_year.to_i != current_year - user_age.to_i && (g_bread == "n" || health_ins == "n")
+	user_status = "Probably a vampire..."
+elsif user_year.to_i == current_year - user_age.to_i && (g_bread == "y" || health_ins == "y")
+	user_status = "Probably not a vampire..."
+end
+
+p "Checking user status for signs of vampire."
 p user_status
 
 
