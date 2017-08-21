42a8fb4ed14d79a41d2e6bfa4b8e2257286bd371
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-08-21 08:20:08 -0700

    submit 5.6

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5]
+
+p arr.grep(6)
+
+# I had no luck finding a partner this week for
+# this challenge.
+# I was unable to find the information to complete
+# the assignment correctly after searching
+# I will do more research and update my assignment submition
+# as soon as possible.
