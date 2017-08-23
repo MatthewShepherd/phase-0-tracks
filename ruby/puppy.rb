002f59e1e81418b98959447defa4f021a2db36ff
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-08-22 17:01:23 -0700

    puppy class submit 6-1

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# Puppy Class
+
+# SPECIES --------------------
+# Miniturous Dogeous
+
+# CHARACTERISTICS ------------
+# Legs: 4
+# Friendly: yes
+# Ears: floppy
+# Nose: wet
+
+# BEHAVIOR -------------------
+# Run
+# Dig
+# Chase tail
+# Lick
+# Bark
