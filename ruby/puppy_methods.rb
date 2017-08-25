a0b43252e65e8e77cc2f64a7577b31aeda71b471
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-08-24 17:34:55 -0700

    commit ruby file for submit

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,39 @@ class Puppy
     toy
   end
 
+  def speak(x)
+  	x.times do 
+  		puts "woof"
+      end
+  	end
+
+  def roll_over
+  	p "rolls over"
+  end
+  
+  def dog_years(x)
+    p x * 7
+  end
+
+  def hand_shake
+  	p "shakes hand"
+  end
+
 end
 
+spot = Puppy.new
+spot.fetch("bone")
+spot.speak(3)
+spot.roll_over
+spot.dog_years(5)
+spot.hand_shake
+
+cat = []
+30.times do
+	cat << Kitten.new
+end
+
+cat.each do |x|
+	p x.meow
+	p x.scratch("table")
+end
