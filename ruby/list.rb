4a925ecf22ff37b0c90ed85059dce550b0c7d299
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-08-30 02:48:30 -0700

    redo 4.2

---
+++ ruby/list.rb
@@ -0,0 +1,12 @@
+class List
+# initialize with stored list items for use
+	def initialize()
+		p "initializing..."
+		@dishes = "do the dishes"
+		@lawn = "mow the lawn"
+	end
+#  
+    def get_items
+    end 
+
+end
