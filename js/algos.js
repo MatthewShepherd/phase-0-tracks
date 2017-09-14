14b9df8def37a38f0e6a138a74119ee87e295c0c
Matthew Shepherd <matthew.g.shepherd@gmail.com>
2017-09-13 22:07:35 -0700

    write function that compares 2 objects

--- js/algos.js
+++ js/algos.js
@@ -26,3 +26,28 @@ var testPhrase2 = ["cat", "building", "course", "idle"];
 var testPhrase = ["one", "five", "eight", "twelve"];
 longestPhrase(testPhrase);
 longestPhrase(testPhrase2);    
+
+function twoObjects(obj1, obj2) {
+	var obj1Arr = [];
+	var obj2Arr = [];
+	for ( var key in obj1) {
+		obj1Arr.push(obj1[key]);
+	}
+	for ( var key in obj2) {
+		obj2Arr.push(obj2[key]);
+	}
+	for ( var i = 0; i < obj1Arr.length; i++) {
+		if ( obj1Arr[i] == obj2Arr[i] ) {
+			console.log('true');
+			return true;
+		}
+	}
+	console.log('false');
+	return false;	
+}
+var myFunction = {color: "red",size: "L"};
+var myFunction2 = {color: "blue",shape: "square"};
+var yourFunction = {name: "Steven", age: 54};
+var yourFunction2 = {name: "Frank", age: 54};
+twoObjects(yourFunction, yourFunction2);
+twoObjects(myFunction, myFunction2);
